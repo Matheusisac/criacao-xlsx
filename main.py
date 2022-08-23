c99377533fca940f954ce3ca4d82ab421275e570
Matheusisac <45320600+Matheusisac@users.noreply.github.com>
2022-08-23 20:46:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import openpyxl
+from openpyxl.styles import Alignment
+
+#Variaveis
+j=3
+paises = []
+capitais = []
+area = []
+moeda = []
+
+#Leitura dos dados e armazenamento dos dados em listas
+request = requests.get("https://restcountries.com/v3.1/all")
+todos = json.loads(request.content)
+for i in range (0,250):
+    paises.append(todos[i]['name']['common'])
+    try:
+      capitais.append(todos[i]['capital'][0])
+    except:
+      capitais.append("-")
+    area.append(todos[i]['area'])
+    try:
+      moeda.append(todos[i]['currencies'].codes)
+    except:
+      moeda.append("-")
+
+#Criação inicial do excel
+excel = openpyxl.load_workbook("dados.xlsx")
+sheet = excel.worksheets[0]
+sheet.merge_cells('A1:D1')
+sheet.cell(row=1,column=1).value = 'Countries List'
+cor = openpyxl.styles.colors.Color(rgb='4F4F4F')
+addcor = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=cor)
+sheet['A1'].fill = addcor
+sheet.cell(row=2,column=1).value = "Name"
+sheet.cell(row=2,column=2).value = 'Capital'
+sheet.cell(row=2,column=3).value = 'Area'
+sheet.cell(row=2,column=4).value = 'Currencies'
+
+cor = openpyxl.styles.colors.Color(rgb='808080')
+addcor = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=cor)
+sheet['A2'].fill = addcor
+sheet['B2'].fill = addcor
+sheet['C2'].fill = addcor
+sheet['D2'].fill = addcor
+
+currentCell = sheet['A1']
+currentCell.alignment = Alignment(horizontal='center')
+currentCell = sheet['A2']
+currentCell.alignment = Alignment(horizontal='center')
+currentCell = sheet['B2']
+currentCell.alignment = Alignment(horizontal='center')
+currentCell = sheet['C2']
+currentCell.alignment = Alignment(horizontal='center')
+currentCell = sheet['D2']
+currentCell.alignment = Alignment(horizontal='center')
+
+#Adicionando os dados lidos
+for i in range (0,250):
+  sheet.cell(row=j,column=1).value = paises[i]
+  sheet.cell(row=j,column=2).value = capitais[i]
+  sheet.cell(row=j,column=3).value = area[i]
+  sheet.cell(row=j,column=4).value = str(moeda[i])
+  j = j + 1
+
+
+excel.save('dados.xlsx')
